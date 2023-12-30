58a5882b8d4777e6e38ccf56b9cefdfeac86bd16
Muktha Meti <132074933+MukthaMeti@users.noreply.github.com>
2023-12-30 20:31:52 +0530

    Add files via upload

---
+++ Java/Question2.java
@@ -0,0 +1,11 @@
+public class Question2 {
+    public static void main(Sring args[]){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println(x:"Enter side of square: ");
+        double side = sc.nextDouble();
+
+        double area = side * side;
+        System.out.println(area);
+    }
+}
